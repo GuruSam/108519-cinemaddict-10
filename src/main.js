1e162e0af1ee54e53db849c68f5927322cd2f769
GuruSam <artem7127.com@inbox.ru>
2019-11-20 20:58:34 +0500

    Фикс попапа

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const filmCardTemplate = () =>
   </article>`;
 
 const filmDetailsTemplate = () =>
-  `<section class="film-details" hidden>
+  `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
       <div class="form-details__top-container">
         <div class="film-details__close">
